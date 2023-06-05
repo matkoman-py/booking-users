fd980d38109632dc67fdd2842fb330f6eec8177d
Mateja Cosovic <mateja@192.168.1.8>
2023-06-05 21:28:21 +0200

    feat: initial setup

---
+++ booking-users/src/main/java/com/bookingusers/BookingUsersApplication.java
@@ -0,0 +1,13 @@
+package com.bookingusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingUsersApplication.class, args);
+	}
+
+}
---
+++ booking-users/src/test/java/com/bookingusers/BookingUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
