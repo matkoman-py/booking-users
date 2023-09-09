b783c0ba328686ed7814ffea9b984ee0625a6d22
markuza99 <petarns99@yahoo.com>
2023-09-09 23:15:41 +0200

    fix: change mapping

--- src/main/java/com/bookingusers/proxy/BookingAccommodationProxy.java
+++ src/main/java/com/bookingusers/proxy/BookingAccommodationProxy.java
@@ -1,6 +1,5 @@
 package com.bookingusers.proxy;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "accommodation-service")
 public interface BookingAccommodationProxy {
-    @GetMapping("api/booking/active-reservations-exist/host/{id}")
+    @GetMapping("api/accommodation/booking/active-reservations-exist/host/{id}")
     Boolean doActiveBookingsExistForHost(@PathVariable String id);
 
-    @GetMapping("api/booking/active-reservations-exist/guest/{id}")
+    @GetMapping("api/accommodation/booking/active-reservations-exist/guest/{id}")
     Boolean doActiveBookingsExistForGuest(@PathVariable String id);
 
     @DeleteMapping("api/accomodation/host/{id}")
     String deleteAllForHost(@PathVariable String id);
 
-
 }
-
